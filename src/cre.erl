7697572ce87e709bf434004bc9d8a101804deeb4
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-02-19 12:58:25 +0100

    Helper functions for translating app into opt list added.

--- src/cre.erl
+++ src/cre.erl
@@ -17,7 +17,7 @@
 % limitations under the License.
 
 -module( cre ).
--author( "Jörgen Brandt <brandjoe@hu-berlin.de>" ).
+-author( "Jorgen Brandt <brandjoe@hu-berlin.de>" ).
 
 -behavior( gen_server ).
 
@@ -28,7 +28,16 @@
 -export( [code_change/3, handle_cast/2, handle_info/2, init/1, terminate/2,
           handle_call/3] ).
 
--export( [start/0, submit/1] ).
+-export( [start/0, submit/1, format_optlist/1, get_optlist/2] ).
+
+
+%% =============================================================================
+%% Includes
+%% =============================================================================
+
+-ifdef( TEST ).
+-include_lib( "eunit/include/eunit.hrl" ).
+-endif.
 
 %% =============================================================================
 %% Abstract Syntax
@@ -41,7 +50,8 @@
 -type lam()     :: {lam, Line::pos_integer(), Name::string(),
                          S::sign(), B::forbody()}.
 -type sign()    :: {sign, Lo::[param()], Li::[param()]}.
--type param()   :: {param, N::string(), Pl::boolean()}.
+-type param()   :: {param, M::name(), Pl::boolean()}.
+-type name()    :: {name, N::string(), Pf::boolean()}.
 -type forbody() :: {forbody, L::lang(), S::string()}.
 -type lang()    :: bash | python | r.
 
@@ -52,15 +62,15 @@
 code_change( _OldVsn, State, _Extra ) -> {ok, State}.
 handle_cast( _Request, State )        -> {noreply, State}.
 handle_info( _Info, State )           -> {noreply, State}.
-init( [] )                            -> {ok, {1}}.
+init( [] )                            -> {ok, {cre_local, 1}}.
 terminate( _Reason, _State )          -> ok.
 
 %% Call Handler %%
 
 handle_call( {submit, A={app, _, _, {lam, _, Name, {sign, Lo, _}, _}, _}},
-             _From, {R} ) ->
+             _From, {Mod, R} ) ->
 
-  _Pid = spawn_link( ?MODULE, stage, [A, R] ),
+  _Pid = spawn_link( Mod, stage, [A, R] ),
 
   {reply, {fut, Name, R, Lo}, {R+1}}.
 
@@ -77,30 +87,71 @@ start() ->
 submit( App ) when is_tuple( App ) ->
   gen_server:call( ?MODULE, {submit, App} ).
 
+
+-spec format_optlist( OptList::[{atom(), _}] ) -> iolist().
+
+format_optlist( OptList ) ->
+  string:join( [format_optpair( OptPair ) || OptPair <- OptList], " " ).
+
+-spec get_optlist( A::app(), R::pos_integer() ) -> [{atom(), _}].
+
+get_optlist( {app, _, _, {lam, _, Name, {sign, Lo, Li}, {forbody, Lang, _}}, Fa}, R ) ->
+  Prefix     = lists:flatten( io_lib:format( "~B", [R] ) ),
+  GeneralOpt = [{lang, Lang}, {prefix, Prefix}, {taskname, Name}],       % general info
+  OutputOpt  = [acc_out( N, Pl ) || {param, {name, N, _}, Pl} <- Lo],    % output names
+  InputOpt   = [acc_in( N, Pl, Fa ) || {param, {name, N, _}, Pl} <- Li], % input parameters
+  FileOpt    = lists:foldl( fun acc_file/2, [], Lo++Li ),                % input and output file declarations
+  GeneralOpt++OutputOpt++InputOpt++FileOpt.
+
 %% =============================================================================
 %% Internal Functions
 %% =============================================================================
 
--spec run( A::app(), R::pos_integer() ) -> ok.
+-spec format_optpair( OptPair::{atom(), _} ) -> iolist().
 
-stage( {app, _, _, {lam, _, Name, {sign, Lo, _}, {forbody, Lang, Script}}, Fa}, R ) ->
+format_optpair( {lang, L} ) -> io_lib:format( "-l ~w", [L] ).
 
-  % create prefix string
-  Prefix = lists:flatten( io_lib:format( "~B", [R] ) ),
+-spec acc_out( N::string(), Pl::boolean() ) -> {atom(), string()}.
 
-  % general info
-  GeneralOpt = [{lang, Lang}, {prefix, Prefix}, {taskname, Name}].
+acc_out( N, Pl ) ->
+  K = case Pl of
+        true -> listout;
+        false -> singout
+      end,
+  {K, N}.
 
-  % output names
-  OutputOpt = [{case Pl of true -> listout; false -> singout end, N} || {param, {name, N, _}}, Pl} <- Lo]
-
-  % output file declarations
-  OutputFileOpt = lists:foldl( fun acc_file/2, [], Lo ),
-
-  % input parameters
-  InputOpt = 
-
+-spec acc_file( A::app(), Acc0::[{atom(), string()}] ) -> [{atom(), string()}].
 
 acc_file( {param, {name, N, Pf}, _}, Acc0 ) when Pf -> [{file, N}|Acc0];
-acc_file( {param, {name, _, _}, _}, Acc0 )          -> Acc0.
+acc_file( _,                         Acc0 )         -> Acc0.
 
+-spec acc_in( N, Pl, Fa ) -> {atom(), string()}
+when N  :: string(),
+     Pl :: boolean(),
+     Fa :: #{string() => [str()]}.
+
+acc_in( N, Pl, Fa ) ->
+  StrList = maps:get( N, Fa ),
+  CommaSeparated = string:join( [S|| {str, S} <- StrList], "," ),
+  V = lists:flatten( io_lib:format( "~s:~s", [N, CommaSeparated] ) ),
+  K = case Pl of
+    true -> listin;
+    false -> singin
+  end,
+  {K, V}.
+
+-ifdef( TEST ).
+
+format_optpair_formats_bash_test() ->
+  S = format_optpair( {lang, bash} ),
+  ?assertEqual( "-l bash", lists:flatten( S ) ).
+
+format_optpair_formats_python_test() ->
+  S = format_optpair( {lang, python} ),
+  ?assertEqual( "-l python", lists:flatten( S ) ).
+
+format_optpair_formats_r_test() ->
+  S = format_optpair( {lang, r} ),
+  ?assertEqual( "-l r", lists:flatten( S ) ).
+
+-endif.
